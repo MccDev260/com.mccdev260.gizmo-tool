31d07b283d59b4992064d35abf950b20544c020e
Jon McCreadie <mccducky23@gmail.com>
2024-04-27 20:33:01 +0100

    Squashed commit of the following:
    commit a96b43b7c69ddffe13d3a7e3e7084f997e0d63de
    Author: Jon McCreadie <mccducky23@gmail.com>
    Date:   Sat Apr 27 20:32:13 2024 +0100
    
        Lil clean.
    
        Moved the namespace and using directives into the #if block.
    
    commit 43f832e652803f0c218f1de9a2f4293e7706f525
    Author: Jon McCreadie <mccducky23@gmail.com>
    Date:   Sat Apr 27 20:10:07 2024 +0100
    
        Update com.mccdev260.gizmo-tool.asmdef
    
        Errors showing up in builds because I forgot to exclude the tool from builds. Fixed now.

--- Editor/GizmoDrawer.cs
+++ Editor/GizmoDrawer.cs
@@ -1,73 +1,73 @@
-using UnityEngine;
-
-namespace MccDev260.GizmoTool
-{
 #if UNITY_EDITOR
-    [ExecuteInEditMode]
-    public class GizmoDrawer : MonoBehaviour
+    using UnityEngine;
+
+    namespace MccDev260.GizmoTool
     {
-        [SerializeField] private bool drawOnSelectOnly;
-
-        #region Properties
-        //Shared
-        [HideInInspector] public Transform originTransform;
-        [HideInInspector] public Vector3 originPos;
-        [HideInInspector] public GizmoType gizmoType;
-        [HideInInspector] public Color gizmoColor = Color.blue;
-
-        //Icon
-        [HideInInspector] public string filePathString;
-        [HideInInspector] public bool allowScaling;
-
-        //Sphere
-        [HideInInspector] public float floatRadius = 1f;
-        //Cube
-        [HideInInspector] public bool uniformScale;
-        [HideInInspector] public Vector3 scale = Vector3.one;
-
-        // Mesh
-        [HideInInspector] public Mesh mesh;
-        [HideInInspector] public Vector3 meshRotation;
-        [HideInInspector] public bool useAttachedTransformValues;
-        [HideInInspector] public bool useMeshOnFilter;
-        //Line
-        [HideInInspector] public Transform targetTransform;
-        [HideInInspector] public Vector3 targetPosition;
-
-        //LineList & LineStrip
-        [HideInInspector] public Vector3[] points = new Vector3[0];
-
-        [HideInInspector] public bool useTransformArray;
-        [HideInInspector] public Transform[] transformPoints = new Transform[0];
-
-        [HideInInspector] public bool looped;
-
-        // GUI Texture
-        /// <summary>
-        /// The size and position of the texture on the "screen" defined by the XY plane.
-        /// </summary>
-        [HideInInspector] public Rect screenRect;
-        /// <summary>
-        /// The texture to be displayed.
-        /// </summary>
-        [HideInInspector] public Texture texture;
-        /// <summary>
-        /// An optional material to apply the texture.
-        /// </summary>
-        [HideInInspector] public Material mat;
-        #endregion
-
-        private void OnDrawGizmosSelected()
+        [ExecuteInEditMode]
+        public class GizmoDrawer : MonoBehaviour
         {
-            if (drawOnSelectOnly)
-                GizmoTool.DrawGizmo(this);
-        }
+            [SerializeField] private bool drawOnSelectOnly;
 
-        private void OnDrawGizmos()
-        {
-            if (!drawOnSelectOnly)
-                GizmoTool.DrawGizmo(this);
+            #region Properties
+            //Shared
+            [HideInInspector] public Transform originTransform;
+            [HideInInspector] public Vector3 originPos;
+            [HideInInspector] public GizmoType gizmoType;
+            [HideInInspector] public Color gizmoColor = Color.blue;
+
+            //Icon
+            [HideInInspector] public string filePathString;
+            [HideInInspector] public bool allowScaling;
+
+            //Sphere
+            [HideInInspector] public float floatRadius = 1f;
+            //Cube
+            [HideInInspector] public bool uniformScale;
+            [HideInInspector] public Vector3 scale = Vector3.one;
+
+            // Mesh
+            [HideInInspector] public Mesh mesh;
+            [HideInInspector] public Vector3 meshRotation;
+            [HideInInspector] public bool useAttachedTransformValues;
+            [HideInInspector] public bool useMeshOnFilter;
+            //Line
+            [HideInInspector] public Transform targetTransform;
+            [HideInInspector] public Vector3 targetPosition;
+
+            //LineList & LineStrip
+            [HideInInspector] public Vector3[] points = new Vector3[0];
+
+            [HideInInspector] public bool useTransformArray;
+            [HideInInspector] public Transform[] transformPoints = new Transform[0];
+
+            [HideInInspector] public bool looped;
+
+            // GUI Texture
+            /// <summary>
+            /// The size and position of the texture on the "screen" defined by the XY plane.
+            /// </summary>
+            [HideInInspector] public Rect screenRect;
+            /// <summary>
+            /// The texture to be displayed.
+            /// </summary>
+            [HideInInspector] public Texture texture;
+            /// <summary>
+            /// An optional material to apply the texture.
+            /// </summary>
+            [HideInInspector] public Material mat;
+            #endregion
+
+            private void OnDrawGizmosSelected()
+            {
+                if (drawOnSelectOnly)
+                    GizmoTool.DrawGizmo(this);
+            }
+
+            private void OnDrawGizmos()
+            {
+                if (!drawOnSelectOnly)
+                    GizmoTool.DrawGizmo(this);
+            }
         }
     }
 #endif
-}
--- Editor/GizmoDrawerInspector.cs
+++ Editor/GizmoDrawerInspector.cs
@@ -1,230 +1,229 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System.Runtime.CompilerServices;
+﻿#if UNITY_EDITOR
+    using UnityEngine;
+    using UnityEditor;
 
-namespace MccDev260.GizmoTool
-{
-#if UNITY_EDITOR
-    [CustomEditor(typeof(GizmoDrawer))]
-    public class GizmoDrawerInspector : Editor
+    namespace MccDev260.GizmoTool
     {
-        bool _hasColour;
-        bool _showSingleOrigin;
-
-        #region SerializedProps
-        SerializedProperty
-            serProp_colour,
-            serProp_originTrans,
-            serProp_originPos,
-            serProp_gizmoType,
-            serProp_floatRadius,
-            serProp_vec3Scale,
-            serProp_filePathString,
-            serProp_allowScalingBool,
-            serProp_mesh,
-            serProp_meshRot,
-            serProp_targetTrans,
-            serProp_targetPos,
-            serProp_linePointArray,
-            serProp_transformLinePointArray,
-            serProp_useTransformArrayBool,
-            serProp_lineLoopedBool,
-            serProp_screenRect,
-            serProp_texture,
-            serProp_mat,
-            serProp_useTransformVals,
-            serProp_useMeshOnFilter;
-        #endregion
-
-        GizmoDrawer gizmoDrawer;
-
-        private void OnEnable()
+        [CustomEditor(typeof(GizmoDrawer))]
+        public class GizmoDrawerInspector : Editor
         {
-            gizmoDrawer = (GizmoDrawer)target;
+            bool _hasColour;
+            bool _showSingleOrigin;
 
-            serProp_colour = serializedObject.FindProperty("gizmoColor");
-            serProp_originTrans = serializedObject.FindProperty("originTransform");
-            serProp_originPos = serializedObject.FindProperty("originPos");
-            serProp_gizmoType = serializedObject.FindProperty("gizmoType");
+            #region SerializedProps
+            SerializedProperty
+                serProp_colour,
+                serProp_originTrans,
+                serProp_originPos,
+                serProp_gizmoType,
+                serProp_floatRadius,
+                serProp_vec3Scale,
+                serProp_filePathString,
+                serProp_allowScalingBool,
+                serProp_mesh,
+                serProp_meshRot,
+                serProp_targetTrans,
+                serProp_targetPos,
+                serProp_linePointArray,
+                serProp_transformLinePointArray,
+                serProp_useTransformArrayBool,
+                serProp_lineLoopedBool,
+                serProp_screenRect,
+                serProp_texture,
+                serProp_mat,
+                serProp_useTransformVals,
+                serProp_useMeshOnFilter;
+            #endregion
 
-            serProp_floatRadius = serializedObject.FindProperty("floatRadius");
+            GizmoDrawer gizmoDrawer;
 
-            serProp_vec3Scale = serializedObject.FindProperty("scale");
-
-            serProp_filePathString = serializedObject.FindProperty("filePathString");
-            serProp_allowScalingBool = serializedObject.FindProperty("allowScaling");
-
-            serProp_mesh = serializedObject.FindProperty("mesh");
-            serProp_meshRot = serializedObject.FindProperty("meshRotation");
-
-            serProp_targetTrans = serializedObject.FindProperty("targetTransform");
-            serProp_targetPos = serializedObject.FindProperty("targetPosition");
-
-            serProp_linePointArray = serializedObject.FindProperty("points");
-            serProp_transformLinePointArray = serializedObject.FindProperty("transformPoints");
-            serProp_useTransformArrayBool = serializedObject.FindProperty("useTransformArray");
-            serProp_lineLoopedBool = serializedObject.FindProperty("looped");
-
-            serProp_screenRect = serializedObject.FindProperty("screenRect");
-            serProp_texture = serializedObject.FindProperty("texture");
-            serProp_mat = serializedObject.FindProperty("mat");
-
-            serProp_useTransformVals = serializedObject.FindProperty("useAttachedTransformValues");
-            serProp_useMeshOnFilter = serializedObject.FindProperty("useMeshOnFilter");
-        }
-
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-
-            serializedObject.Update();
-
-            EditorGUILayout.Space();
-
-            // Type
-            EditorGUILayout.PropertyField(serProp_gizmoType, true);
-            EditorGUILayout.Space();
-
-            if (_showSingleOrigin && ShowOriginOptions(gizmoDrawer))
+            private void OnEnable()
             {
-                // Transform
-                EditorGUILayout.PropertyField(serProp_originTrans);
+                gizmoDrawer = (GizmoDrawer)target;
 
-                if (gizmoDrawer.originTransform == null)
+                serProp_colour = serializedObject.FindProperty("gizmoColor");
+                serProp_originTrans = serializedObject.FindProperty("originTransform");
+                serProp_originPos = serializedObject.FindProperty("originPos");
+                serProp_gizmoType = serializedObject.FindProperty("gizmoType");
+
+                serProp_floatRadius = serializedObject.FindProperty("floatRadius");
+
+                serProp_vec3Scale = serializedObject.FindProperty("scale");
+
+                serProp_filePathString = serializedObject.FindProperty("filePathString");
+                serProp_allowScalingBool = serializedObject.FindProperty("allowScaling");
+
+                serProp_mesh = serializedObject.FindProperty("mesh");
+                serProp_meshRot = serializedObject.FindProperty("meshRotation");
+
+                serProp_targetTrans = serializedObject.FindProperty("targetTransform");
+                serProp_targetPos = serializedObject.FindProperty("targetPosition");
+
+                serProp_linePointArray = serializedObject.FindProperty("points");
+                serProp_transformLinePointArray = serializedObject.FindProperty("transformPoints");
+                serProp_useTransformArrayBool = serializedObject.FindProperty("useTransformArray");
+                serProp_lineLoopedBool = serializedObject.FindProperty("looped");
+
+                serProp_screenRect = serializedObject.FindProperty("screenRect");
+                serProp_texture = serializedObject.FindProperty("texture");
+                serProp_mat = serializedObject.FindProperty("mat");
+
+                serProp_useTransformVals = serializedObject.FindProperty("useAttachedTransformValues");
+                serProp_useMeshOnFilter = serializedObject.FindProperty("useMeshOnFilter");
+            }
+
+            public override void OnInspectorGUI()
+            {
+                base.OnInspectorGUI();
+
+                serializedObject.Update();
+
+                EditorGUILayout.Space();
+
+                // Type
+                EditorGUILayout.PropertyField(serProp_gizmoType, true);
+                EditorGUILayout.Space();
+
+                if (_showSingleOrigin && ShowOriginOptions(gizmoDrawer))
                 {
-                    EditorGUILayout.PropertyField(serProp_originPos);
+                    // Transform
+                    EditorGUILayout.PropertyField(serProp_originTrans);
+
+                    if (gizmoDrawer.originTransform == null)
+                    {
+                        EditorGUILayout.PropertyField(serProp_originPos);
+                    }
+
+                    EditorGUILayout.Space();
                 }
 
-                EditorGUILayout.Space();
-            }
-
-            if (_hasColour)
-            {
-                EditorGUILayout.PropertyField(serProp_colour);
-                EditorGUILayout.Space();
-            }
-
-            DrawInspector(gizmoDrawer.gizmoType, this, out _hasColour, out _showSingleOrigin);
-
-            serializedObject.ApplyModifiedProperties();
-
-            if (GUI.changed)
-                SceneView.RepaintAll();
-        }
-
-        static void DrawInspector(GizmoType type, GizmoDrawerInspector editor, out bool showColour, out bool showSingleOrigin)
-        {
-            showSingleOrigin = false;
-            showColour = false;
-
-            switch (type)
-            {
-                case GizmoType.Sphere or GizmoType.WireSphere:
-                    showSingleOrigin = true;
-
-                    showColour = true;
-                    EditorGUILayout.PropertyField(editor.serProp_floatRadius);
-                    break;
-
-                case GizmoType.Cube or GizmoType.WireCube:
-                    showSingleOrigin = true;
-
-                    showColour = true;
-                    EditorGUILayout.PropertyField(editor.serProp_vec3Scale);
-                    break;
-
-                case GizmoType.Icon:
-                    showSingleOrigin = true;
-                    showColour = true;
-
-                    EditorGUILayout.PropertyField(editor.serProp_filePathString);
-                    EditorGUILayout.PropertyField(editor.serProp_allowScalingBool);
-                    break;
-
-                case GizmoType.Mesh or GizmoType.WireMesh:
-                    showSingleOrigin = true;
-                    showColour = true;
-
-                    if (ShowMeshOptions(editor.gizmoDrawer))
-                    {
-                        EditorGUILayout.PropertyField(editor.serProp_mesh);
-                    }
-
-                    if (editor.gizmoDrawer.transform.GetComponent<MeshFilter>())
-                    {
-                        EditorGUILayout.PropertyField(editor.serProp_useMeshOnFilter);
-                    }
-
-                    EditorGUILayout.PropertyField(editor.serProp_useTransformVals);
-
-                    if (editor.serProp_useTransformVals.boolValue == true) return;
-
-                    EditorGUILayout.PropertyField(editor.serProp_meshRot);
-                    EditorGUILayout.PropertyField(editor.serProp_vec3Scale);
-                    break;
-
-                case GizmoType.Line:
-                    showSingleOrigin = true;
-                    showColour = true;
-
-
-                    EditorGUILayout.PropertyField(editor.serProp_targetTrans);
-
-                    if (editor.gizmoDrawer.targetTransform == null)
-                    {
-                        EditorGUILayout.PropertyField(editor.serProp_targetPos);
-                    }
-                    break;
-
-                case GizmoType.LineList or GizmoType.LineStrip:
-                    showSingleOrigin = false;
-                    showColour = true;
-
-                    EditorGUILayout.PropertyField(editor.serProp_useTransformArrayBool);
-
-                    if (type == GizmoType.LineStrip)
-                        EditorGUILayout.PropertyField(editor.serProp_lineLoopedBool);
-
-                    if (editor.gizmoDrawer.useTransformArray)
-                    {
-                        EditorGUILayout.PropertyField(editor.serProp_transformLinePointArray);
-                    }
-                    else
-                    {
-                        EditorGUILayout.PropertyField(editor.serProp_linePointArray, true);
-                    }
-                    break;
-
-                case GizmoType.GuiTexture:
-                    showSingleOrigin = true;
-                    showColour = false;
-                    EditorGUILayout.PropertyField(editor.serProp_texture);
+                if (_hasColour)
+                {
+                    EditorGUILayout.PropertyField(serProp_colour);
                     EditorGUILayout.Space();
-                    EditorGUILayout.PropertyField(editor.serProp_screenRect);
-                    EditorGUILayout.Space();
-                    EditorGUILayout.PropertyField(editor.serProp_mat);
-                    break;
-            }
-        }
+                }
 
-        static bool ShowOriginOptions(GizmoDrawer drawer)
-        {
-            if (drawer.gizmoType == GizmoType.Mesh || drawer.gizmoType == GizmoType.WireMesh)
+                DrawInspector(gizmoDrawer.gizmoType, this, out _hasColour, out _showSingleOrigin);
+
+                serializedObject.ApplyModifiedProperties();
+
+                if (GUI.changed)
+                    SceneView.RepaintAll();
+            }
+
+            static void DrawInspector(GizmoType type, GizmoDrawerInspector editor, out bool showColour, out bool showSingleOrigin)
             {
-                if (drawer.useAttachedTransformValues) 
+                showSingleOrigin = false;
+                showColour = false;
+
+                switch (type)
+                {
+                    case GizmoType.Sphere or GizmoType.WireSphere:
+                        showSingleOrigin = true;
+
+                        showColour = true;
+                        EditorGUILayout.PropertyField(editor.serProp_floatRadius);
+                        break;
+
+                    case GizmoType.Cube or GizmoType.WireCube:
+                        showSingleOrigin = true;
+
+                        showColour = true;
+                        EditorGUILayout.PropertyField(editor.serProp_vec3Scale);
+                        break;
+
+                    case GizmoType.Icon:
+                        showSingleOrigin = true;
+                        showColour = true;
+
+                        EditorGUILayout.PropertyField(editor.serProp_filePathString);
+                        EditorGUILayout.PropertyField(editor.serProp_allowScalingBool);
+                        break;
+
+                    case GizmoType.Mesh or GizmoType.WireMesh:
+                        showSingleOrigin = true;
+                        showColour = true;
+
+                        if (ShowMeshOptions(editor.gizmoDrawer))
+                        {
+                            EditorGUILayout.PropertyField(editor.serProp_mesh);
+                        }
+
+                        if (editor.gizmoDrawer.transform.GetComponent<MeshFilter>())
+                        {
+                            EditorGUILayout.PropertyField(editor.serProp_useMeshOnFilter);
+                        }
+
+                        EditorGUILayout.PropertyField(editor.serProp_useTransformVals);
+
+                        if (editor.serProp_useTransformVals.boolValue == true) return;
+
+                        EditorGUILayout.PropertyField(editor.serProp_meshRot);
+                        EditorGUILayout.PropertyField(editor.serProp_vec3Scale);
+                        break;
+
+                    case GizmoType.Line:
+                        showSingleOrigin = true;
+                        showColour = true;
+
+
+                        EditorGUILayout.PropertyField(editor.serProp_targetTrans);
+
+                        if (editor.gizmoDrawer.targetTransform == null)
+                        {
+                            EditorGUILayout.PropertyField(editor.serProp_targetPos);
+                        }
+                        break;
+
+                    case GizmoType.LineList or GizmoType.LineStrip:
+                        showSingleOrigin = false;
+                        showColour = true;
+
+                        EditorGUILayout.PropertyField(editor.serProp_useTransformArrayBool);
+
+                        if (type == GizmoType.LineStrip)
+                            EditorGUILayout.PropertyField(editor.serProp_lineLoopedBool);
+
+                        if (editor.gizmoDrawer.useTransformArray)
+                        {
+                            EditorGUILayout.PropertyField(editor.serProp_transformLinePointArray);
+                        }
+                        else
+                        {
+                            EditorGUILayout.PropertyField(editor.serProp_linePointArray, true);
+                        }
+                        break;
+
+                    case GizmoType.GuiTexture:
+                        showSingleOrigin = true;
+                        showColour = false;
+                        EditorGUILayout.PropertyField(editor.serProp_texture);
+                        EditorGUILayout.Space();
+                        EditorGUILayout.PropertyField(editor.serProp_screenRect);
+                        EditorGUILayout.Space();
+                        EditorGUILayout.PropertyField(editor.serProp_mat);
+                        break;
+                }
+            }
+
+            static bool ShowOriginOptions(GizmoDrawer drawer)
+            {
+                if (drawer.gizmoType == GizmoType.Mesh || drawer.gizmoType == GizmoType.WireMesh)
+                {
+                    if (drawer.useAttachedTransformValues) 
+                        return false;
+                }
+
+                return true;
+            }
+
+            static bool ShowMeshOptions(GizmoDrawer drawer)
+            {
+                if (drawer.GetComponent<MeshFilter>() && drawer.useMeshOnFilter)
                     return false;
+
+                return true;
             }
-
-            return true;
-        }
-
-        static bool ShowMeshOptions(GizmoDrawer drawer)
-        {
-            if (drawer.GetComponent<MeshFilter>() && drawer.useMeshOnFilter)
-                return false;
-
-            return true;
         }
     }
 #endif
-}
