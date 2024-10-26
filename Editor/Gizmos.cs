441cf2542e70a1c904854a0a617193d9c49781d9
Jon McCreadie <mccducky23@gmail.com>
2024-10-26 16:31:57 +0100

    Update Gizmos.cs
    Changed redundant access modifier to match class.

--- Editor/Gizmos.cs
+++ Editor/Gizmos.cs
@@ -6,25 +6,25 @@ namespace MccDev260.GizmoTool
 {
     internal static class Gizmos
     {
-        public static void DrawSphere(Vector3 worldPos, float radius, Color colour)
+        internal static void DrawSphere(Vector3 worldPos, float radius, Color colour)
         {
             UnityEngine.Gizmos.color = colour;
             UnityEngine.Gizmos.DrawSphere(worldPos, radius);
         }
 
-        public static void DrawWireSphere(Vector3 worldPos, float radius, Color colour)
+        internal static void DrawWireSphere(Vector3 worldPos, float radius, Color colour)
         {
             UnityEngine.Gizmos.color = colour;
             UnityEngine.Gizmos.DrawWireSphere(worldPos, radius);
         }
 
-        public static void DrawCube(Vector3 position, Vector3 size, Color colour)
+        internal static void DrawCube(Vector3 position, Vector3 size, Color colour)
         {
             UnityEngine.Gizmos.color = colour;
             UnityEngine.Gizmos.DrawCube(position, size);
         }
 
-        public static void DrawWireCube(Vector3 position, Vector3 size, Color colour)
+        internal static void DrawWireCube(Vector3 position, Vector3 size, Color colour)
         {
             UnityEngine.Gizmos.color = colour;
             UnityEngine.Gizmos.DrawWireCube(position, size);
@@ -38,30 +38,30 @@ namespace MccDev260.GizmoTool
         /// <param name="position">Location of the icon in world space.</param>
         /// <param name="fileName">File should be placed in the Assets/Gizmos folder.</param>
         /// <param name="allowScaling">Determines if the icon is allowed to be scaled.</param>
-        public static void DrawIcon(Vector3 position, string fileName, bool allowScaling, Color colour) 
+        internal static void DrawIcon(Vector3 position, string fileName, bool allowScaling, Color colour) 
         {
             UnityEngine.Gizmos.DrawIcon(position, fileName, allowScaling, colour);
         }
 
-        public static void DrawMesh(Mesh mesh, Vector3 position, Quaternion rotation, Vector3 scale, Color colour)
+        internal static void DrawMesh(Mesh mesh, Vector3 position, Quaternion rotation, Vector3 scale, Color colour)
         {
             UnityEngine.Gizmos.color = colour;
             UnityEngine.Gizmos.DrawMesh(mesh, position, rotation, scale);
         }
 
-        public static void DrawWireMesh(Mesh mesh, Vector3 position, Quaternion rotation, Vector3 scale, Color colour)
+        internal static void DrawWireMesh(Mesh mesh, Vector3 position, Quaternion rotation, Vector3 scale, Color colour)
         {
             UnityEngine.Gizmos.color = colour;
             UnityEngine.Gizmos.DrawWireMesh(mesh, position, rotation, scale);
         }
 
-        public static void DrawLine(Vector3 startPos, Vector3 endPos, Color colour)
+        internal static void DrawLine(Vector3 startPos, Vector3 endPos, Color colour)
         {
             UnityEngine.Gizmos.color = colour;
             UnityEngine.Gizmos.DrawLine(startPos, endPos);
         }
 
-        public static void DrawLineList(Vector3[] points, Color colour)
+        internal static void DrawLineList(Vector3[] points, Color colour)
         {
             UnityEngine.Gizmos.color = colour;
             try
@@ -74,13 +74,13 @@ namespace MccDev260.GizmoTool
             }
         }
 
-        public static void DrawLineStrip(Vector3[] points, Color colour, bool looped)
+        internal static void DrawLineStrip(Vector3[] points, Color colour, bool looped)
         {
             UnityEngine.Gizmos.color = colour;
             UnityEngine.Gizmos.DrawLineStrip(points, looped);
         }
 
-        public static void DrawGuiTexture(Rect screenRect, Texture texture, Material mat = null)
+        internal static void DrawGuiTexture(Rect screenRect, Texture texture, Material mat = null)
         {
             UnityEngine.Gizmos.DrawGUITexture(screenRect, texture, mat);
         }
